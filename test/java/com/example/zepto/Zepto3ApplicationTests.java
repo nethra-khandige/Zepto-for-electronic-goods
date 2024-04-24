0f6aab87aba132797d15e3c55d586b141b5adf60
Nethra Khandige <106540489+nethra-khandige@users.noreply.github.com>
2024-04-24 12:47:42 +0530

    Add files via upload

---
+++ test/java/com/example/zepto/Zepto3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zepto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zepto3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
